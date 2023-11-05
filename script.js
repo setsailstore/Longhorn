0bd3bdf4581ff98a81df6a7029cd2f6fff81d658
setsailstore <149674371+setsailstore@users.noreply.github.com>
2023-11-05 14:54:59 +0700

    Project

---
+++ script.js
@@ -0,0 +1,17 @@
+const navigasi = document.querySelector ('.navbar');
+const closemenu = document.querySelector ('.close');
+const openmenu = document.querySelector ('.mobile');
+
+openmenu.addEventListener('click',show);
+closemenu.addEventListener('click',close);
+
+function show(){
+    navigasi.style.display = 'flex';
+    navigasi.style.top = '0';
+    navigasi.style.transition =  "top 1s ease";
+
+}
+
+function close(){
+    navigasi.style.top = '-100%';
+}
